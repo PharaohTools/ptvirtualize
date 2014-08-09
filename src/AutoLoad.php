a3cc69975f7010dcc45163abca019ef6313fc5af
David Amanshia <phpengine@hotmail.co.uk>
2014-08-09 23:57:06 +0100

    autoload updated to match current cleo

--- src/AutoLoad.php
+++ src/AutoLoad.php
@@ -12,42 +12,42 @@ class AutoLoader{
         $classNameForLoad = str_replace('\\' , DIRECTORY_SEPARATOR, $className);
         $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $classNameForLoad.'.php';
         if (is_readable($filename)) {
-          require_once $filename;
-          return; }
+            require_once $filename;
+            return; }
         // look in Extensions, and then in core modules, so that an extension version will load first
         // also finally looks in Core, but only Base should be in there
         $allModuleParentDirectories = array("Extensions", "Modules", "Core");
         foreach ($allModuleParentDirectories as $oneModuleParentDirectory) {
-          $currentModulesParentDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . $oneModuleParentDirectory ;
-          $modulesIndividualDirectories = scandir($currentModulesParentDir);
-          foreach ($modulesIndividualDirectories as $singleModuleDir) {
-            if (!in_array($singleModuleDir, array(".", ".."))) { // if not dot or double dot
-              if ( is_dir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir)) { // if is a dir
-                $classNameForLoad = str_replace('\\' , DIRECTORY_SEPARATOR, $className);
-                $filename =
-                  $currentModulesParentDir . DIRECTORY_SEPARATOR . $singleModuleDir .
-                  DIRECTORY_SEPARATOR . $classNameForLoad.'.php';
-                if (is_readable($filename)) {
-                  require_once $filename;
-                  return; } } } } }
+            $currentModulesParentDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . $oneModuleParentDirectory ;
+            $modulesIndividualDirectories = scandir($currentModulesParentDir);
+            foreach ($modulesIndividualDirectories as $singleModuleDir) {
+                if (!in_array($singleModuleDir, array(".", ".."))) { // if not dot or double dot
+                    if ( is_dir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir)) { // if is a dir
+                        $classNameForLoad = str_replace('\\' , DIRECTORY_SEPARATOR, $className);
+                        $filename =
+                            $currentModulesParentDir . DIRECTORY_SEPARATOR . $singleModuleDir .
+                            DIRECTORY_SEPARATOR . $classNameForLoad.'.php';
+                        if (is_readable($filename)) {
+                            require_once $filename;
+                            return; } } } } }
     }
 
     public static function getInfoObjects() {
-      $allInfoObjects = array();
-      $allModuleParentDirectories = array("Extensions", "Modules", "Core");
-      foreach ($allModuleParentDirectories as $oneModuleParentDirectory) {
-        $currentModulesParentDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . $oneModuleParentDirectory ;
-        $modulesIndividualDirectories = scandir($currentModulesParentDir);
-        foreach ($modulesIndividualDirectories as $singleModuleDir) {
-          if (!in_array($singleModuleDir, array(".", ".."))) { // if not dot or double dot
-            if ( is_dir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir)) { // if is a dir
-              $filesInModuleDirectory = scandir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir);
-              foreach ($filesInModuleDirectory as $fileInModuleDirectory) {
-                if (substr($fileInModuleDirectory, 0, 5) == "info.") {
-                  require_once $currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir.DIRECTORY_SEPARATOR.$fileInModuleDirectory;
-                  $className = '\Info\\'.$singleModuleDir.'Info' ;
-                  $allInfoObjects[] = new $className(); } } } } } }
-      return $allInfoObjects;
+        $allInfoObjects = array();
+        $allModuleParentDirectories = array("Extensions", "Modules", "Core");
+        foreach ($allModuleParentDirectories as $oneModuleParentDirectory) {
+            $currentModulesParentDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . $oneModuleParentDirectory ;
+            $modulesIndividualDirectories = scandir($currentModulesParentDir);
+            foreach ($modulesIndividualDirectories as $singleModuleDir) {
+                if (!in_array($singleModuleDir, array(".", ".."))) { // if not dot or double dot
+                    if ( is_dir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir)) { // if is a dir
+                        $filesInModuleDirectory = scandir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir);
+                        foreach ($filesInModuleDirectory as $fileInModuleDirectory) {
+                            if (substr($fileInModuleDirectory, 0, 5) == "info.") {
+                                require_once $currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir.DIRECTORY_SEPARATOR.$fileInModuleDirectory;
+                                $className = '\Info\\'.$singleModuleDir.'Info' ;
+                                $allInfoObjects[] = new $className(); } } } } } }
+        return $allInfoObjects;
     }
 
     public static function getSingleInfoObject($module) {
@@ -77,7 +77,7 @@ class AutoLoader{
                 if (!in_array($singleModuleDir, array(".", ".."))) { // if not dot or double dot
                     if ( is_dir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir) && $singleModuleDir == $module ) { // if dirname is module were looking for
                         $filesInModelDirectory = scandir($currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir
-                            .DIRECTORY_SEPARATOR.'Model');
+                        .DIRECTORY_SEPARATOR.'Model');
                         foreach ($filesInModelDirectory as $fileInModelDirectory) {
                             if (!in_array($fileInModelDirectory, array(".", "..")) ) {
                                 $fileToRequire = $currentModulesParentDir.DIRECTORY_SEPARATOR.$singleModuleDir .DIRECTORY_SEPARATOR.'Model'.DIRECTORY_SEPARATOR.$fileInModelDirectory;
@@ -87,6 +87,17 @@ class AutoLoader{
                                 $modelsToReturn[] = new $className($modelParams) ;
                             } } } } } }
         return $modelsToReturn ;
+    }
+
+    public static function moduleExists($module) {
+        $allModuleParentDirectories = array("Extensions", "Modules", "Core");
+        foreach ($allModuleParentDirectories as $oneModuleParentDirectory) {
+            $currentModulesParentDir = dirname(__FILE__) . DIRECTORY_SEPARATOR . $oneModuleParentDirectory ;
+            $modulesIndividualDirectories = scandir($currentModulesParentDir);
+            foreach ($modulesIndividualDirectories as $singleModuleDir) {
+                if ($singleModuleDir == $module) {
+                    return true; } } }
+        return false ;
     }
 
 }
