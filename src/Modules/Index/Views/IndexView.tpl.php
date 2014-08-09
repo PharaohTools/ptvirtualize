db9f6f14169629cfea27967ac58c54ba87c1cd8e
David Amanshia <phpengine@hotmail.co.uk>
2014-08-09 23:13:29 +0100

    indexview correct app name

--- src/Modules/Index/Views/IndexView.tpl.php
+++ src/Modules/Index/Views/IndexView.tpl.php
@@ -1,14 +1,9 @@
-Testingkamen by Golden Contact Computing
+Phlagrant by Golden Contact Computing
 -------------------
 
 About:
 -----------------
-Testingkamen is for Test Automation. It can be used to generate starter test suites for your applications,
-and automated test execution scripts within minutes.
-
-By providing an common API by which to execute tests in a wide range
-of languages and test tools, you can run complex test suites across a range of platforms with little to no
-extra configuration.
+Phlagrant is for controlling Virtual Machines in Development Environments.
 
 -------------------------------------------------------------
 
