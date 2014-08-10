f5a58c661180e42d5d2a9472cd650950687a883c
David Amanshia <phpengine@hotmail.co.uk>
2014-08-10 20:33:10 +0100

    template for tiny-box updated the set variables, more streamlined

--- src/Modules/Flirtify/Templates/tiny-box.php
+++ src/Modules/Flirtify/Templates/tiny-box.php
@@ -16,10 +16,7 @@ class Phlagrantfile extends PhlagrantfileBase {
         $this->config["vm"]["name"] = "daves_box" ;
         $this->config["vm"]["box"] = "VanillaUbuntu_14.04" ;
         $this->config["vm"]["memory"] = "1024" ;
-
-        // These below are part of the box i think
-        $this->config["vm"]["hdd_size"] = "VanillaUbuntu_14.04" ;
-        $this->config["vm"]["cpu_count"] = "VanillaUbuntu_14.04" ;
+        $this->config["vm"]["cpus"] = 1 ;
     }
 
 }
