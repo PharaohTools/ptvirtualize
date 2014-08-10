58403563dae49997910286013f7b1b87ea404308
David Amanshia <phpengine@hotmail.co.uk>
2014-08-11 00:48:45 +0100

    privuusuiner

--- src/Modules/Flirtify/Templates/tiny-box.php
+++ src/Modules/Flirtify/Templates/tiny-box.php
@@ -16,6 +16,12 @@ class Phlagrantfile extends PhlagrantfileBase {
         $this->config["vm"]["name"] = "phlagrant-box" ;
         $this->config["vm"]["box"] = "VanillaUbuntu_14.04" ;
         $this->config["vm"]["memory"] = "1024" ;
+        // Shared folder - This should map to the workstation environment vhost path parent...
+        $this->config["vm"]["shared_folders"][] = array("1024") ;
+        // Networking - maybe use array constants for defaults or something?
+        $this->config["vm"]["networks"][] = array("1024") ;
+        // Provisioning
+        $this->config["vm"]["provision"][] = array("cleopatra" => "") ;
     }
 
 }
