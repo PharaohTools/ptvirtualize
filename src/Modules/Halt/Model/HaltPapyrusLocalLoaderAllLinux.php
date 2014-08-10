1846878ca8ab56eda6707de45e140a17520eed07
David Amanshia <phpengine@hotmail.co.uk>
2014-08-10 15:55:01 +0100

    file loaders for halt

---
+++ src/Modules/Halt/Model/HaltPapyrusLocalLoaderAllLinux.php
@@ -0,0 +1,24 @@
+<?php
+
+Namespace Model;
+
+class HaltPapyrusLocalLoaderAllLinux extends BaseLinuxApp {
+
+    // Compatibility
+    public $os = array("Linux") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("PapyrusLocalLoader") ;
+
+    public function load() {
+        $pf = \AppConfig::getProjectVariable("phlagrant-box", true) ;
+        if (is_array($pf) && count($pf)>0) {
+            return $pf ; }
+        return $pf ;
+    }
+
+}
