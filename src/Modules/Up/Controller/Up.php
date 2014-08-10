fd2f9242f1875a3b3f2759c8389bcaf3050d7ae8
David Amanshia <phpengine@hotmail.co.uk>
2014-08-10 16:44:48 +0100

    minor formatiing on up controller

--- src/Modules/Up/Controller/Up.php
+++ src/Modules/Up/Controller/Up.php
@@ -4,4 +4,26 @@ Namespace Controller ;
 
 class Up extends Base {
 
+    public function execute($pageVars) {
+
+        $thisModel = $this->getModelAndCheckDependencies(substr(get_class($this), 11), $pageVars) ;
+        // if we don't have an object, its an array of errors
+        if (is_array($thisModel)) { return $this->failDependencies($pageVars, $this->content, $thisModel) ; }
+
+        $action = $pageVars["route"]["action"];
+
+        if ($action=="help") {
+            $helpModel = new \Model\Help();
+            $this->content["helpData"] = $helpModel->getHelpData($pageVars["route"]["control"]);
+            return array ("type"=>"view", "view"=>"help", "pageVars"=>$this->content); }
+
+        if ($action=="now") {
+            $this->content["result"] = $thisModel->doUp();
+            return array ("type"=>"view", "view"=>"up", "pageVars"=>$this->content); }
+
+        $this->content["messages"][] = "Invalid Up Action";
+        return array ("type"=>"control", "control"=>"index", "pageVars"=>$this->content);
+
+    }
+
 }
