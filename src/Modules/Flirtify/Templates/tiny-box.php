1cea5ba423869e58c10502beeda9da772945e75f
David Amanshia <phpengine@hotmail.co.uk>
2014-08-11 14:22:53 +0100

    tiny box added default cleofy phlagrant cm

--- src/Modules/Flirtify/Templates/tiny-box.php
+++ src/Modules/Flirtify/Templates/tiny-box.php
@@ -16,12 +16,17 @@ class Phlagrantfile extends PhlagrantfileBase {
         $this->config["vm"]["name"] = "phlagrant-box" ;
         $this->config["vm"]["box"] = "VanillaUbuntu_14.04" ;
         $this->config["vm"]["memory"] = "1024" ;
+        $this->config["ssh"]["username"] = "phlagrant" ;
+        $this->config["ssh"]["password"] = "phlagrant" ;
         // Shared folder - This should map to the workstation environment vhost path parent...
-        $this->config["vm"]["shared_folders"][] = array("1024") ;
+        // $this->config["vm"]["shared_folders"][] = array("1024") ;
         // Networking - maybe use array constants for defaults or something?
-        $this->config["vm"]["networks"][] = array("1024") ;
+        // $this->config["vm"]["networks"][] = array("1024") ;
         // Provisioning
-        $this->config["vm"]["provision"][] = array("cleopatra" => "") ;
+        $this->config["vm"]["provision"][] = array(
+            "group" => "PharoahTools",
+            "Provisioner" => "cleopatra",
+            "script" => "/var/www/pharoah-tools/build/config/phlagrant/" ) ;
     }
 
 }
