2e2f5dbb28d9e88550b374ce3c65a28a9226b84c
David Amanshia <phpengine@hotmail.co.uk>
2014-08-12 00:29:02 +0100

    removed old override from tkmn for getModel

--- src/Modules/Box/Model/Box.php
+++ src/Modules/Box/Model/Box.php
@@ -4,10 +4,4 @@ Namespace Model;
 
 class Box extends BaseModelFactory {
 
-    public static function getModel($params) {
-        $thisModule = substr(get_called_class(), 6) ;
-        $model = \Model\SystemDetectionFactory::getCompatibleModel($thisModule, "Initializer", $params);
-        return $model;
-    }
-
 }
