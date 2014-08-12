0412e4473eb379cb73289542359b0021ad2b4cda
David Amanshia <phpengine@hotmail.co.uk>
2014-08-12 13:21:43 +0100

    cleo bootstrap added

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -10,20 +10,27 @@ $bootStrap->main($bootStrapParams);
 
 class BootStrap {
 
+    private static $exitCode ;
+
     public function __construct() {
         require_once("AutoLoad.php");
         $autoLoader = new autoLoader();
         $autoLoader->launch();
     }
 
-    public function main($argv_or_null) {
-      $routeObject = new \Core\Router();
-      $route = $routeObject->run($argv_or_null);
-      $emptyPageVars = array("messages"=>array(), "route"=>$route);
-      $this->executeControl($route["control"], $emptyPageVars);
+    public static function setExitCode($exitCode){
+        self::$exitCode = $exitCode ;
     }
 
-    private function executeControl($controlToExecute, $pageVars=null) {
+    public function main($argv_or_boot_params_null) {
+        $routeObject = new \Core\Router();
+        $route = $routeObject->run($argv_or_boot_params_null);
+        $emptyPageVars = array("messages"=>array(), "route"=>$route);
+        $this->executeControl($route["control"], $emptyPageVars);
+        $this->exitGracefully();
+    }
+
+    public function executeControl($controlToExecute, $pageVars=null) {
         $control = new \Core\Control();
         $controlResult = $control->executeControl($controlToExecute, $pageVars);
         try {
@@ -38,6 +45,18 @@ class BootStrap {
     private function executeView($viewTemplate, $viewVars) {
         $view = new \Core\View();
         $view->executeView($viewTemplate, $viewVars);
+    }
+
+    private function exitGracefully() {
+        // @note this must be the last executed line as it sets exit code
+        if (self::$exitCode == null) {
+            exit(0) ; }
+        else if (!is_int(self::$exitCode)) {
+            echo "[Pharaoh Exit] Non Integer Exit Code Attempted\n" ;
+            exit(1) ; }
+        else {
+            echo "[Pharaoh Exit] Exiting with exit code: ".self::$exitCode."\n" ;
+            exit(self::$exitCode) ; }
     }
 
 }
