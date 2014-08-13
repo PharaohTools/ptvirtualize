8fa65aaa37009bebce9ace6da36f8ec88c612988
David Amanshia <phpengine@hotmail.co.uk>
2014-08-13 03:18:14 +0100

    added vbox box remove model

---
+++ src/Modules/Virtualbox/Model/VirtualboxBoxRemove.php
@@ -0,0 +1,63 @@
+<?php
+
+Namespace Model;
+
+class VirtualboxBoxRemove extends BaseVirtualboxAllOS {
+
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("BoxRemove") ;
+
+    public function removeBox($target, $name) {
+        // add the box here
+        // remove the directory for the box
+        $this->removeBoxDirectory($target, $name) ;
+        $this->completion() ;
+    }
+
+    protected function askForBoxRemoveExecute() {
+        if (isset($this->params["yes"]) && $this->params["yes"]==true) { return true ; }
+        $question = 'Remove Virtualbox Server Boxes?';
+        return self::askYesOrNo($question);
+    }
+
+    protected function removeBoxDirectory($target, $name) {
+        $boxdir = $target . '/' . $name ;
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params) ;
+        $command = "whoami" ;
+        $whoami = self::executeAndLoad($command);
+        $whoami = str_replace("\n", "", $whoami);
+        $whoami = str_replace("\r", "", $whoami);
+        if (file_exists($boxdir)) {
+            $logging->log("Files exist at $boxdir. Removing.");
+            $command = "rm -rf $boxdir" ;
+            self::executeAndOutput($command); }
+        else {
+            $logging->log("No files exist at $boxdir. Nothing to do, exiting...");
+            return ; }
+        if (!file_exists($target)) {
+            $logging->log("$boxdir removed."); }
+        else {
+            $logging->log("An error occured removing $boxdir. Retrying as superuser");
+            $command = "sudo rm -rf $boxdir" ;
+            self::executeAndOutput($command);
+            if (!file_exists($boxdir)) {
+                $logging->log("$boxdir removed."); }
+            else {
+                $logging->log("Errors occured when removing $boxdir as both $whoami and superuser"); } }
+    }
+
+    protected function completion() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params) ;
+        $logging->log("Completed Removing Box...");
+    }
+
+}
