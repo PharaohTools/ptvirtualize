05f11918e589aa8ea4d2747738218dd453498768
David Amanshia <phpengine@hotmail.co.uk>
2014-08-18 15:51:50 +0100

    halt now tries soft power then ssh

--- src/Modules/Halt/Model/HaltAllLinux.php
+++ src/Modules/Halt/Model/HaltAllLinux.php
@@ -24,8 +24,48 @@ class HaltAllLinux extends BaseLinuxApp {
 
     public function haltNow() {
         $this->loadFiles();
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $logging->log("Attempting soft power off by button...") ;
+        $logging->log("Waiting up to {$this->phlagrantfile->config["vm"]["graceful_halt_timeout"]} seconds for machine to power off...") ;
         $command = "vboxmanage controlvm {$this->phlagrantfile->config["vm"]["name"]} acpipowerbutton" ;
         $this->executeAndOutput($command);
+        if ($this->waitForStatus("poweredoff", $this->phlagrantfile->config["vm"]["graceful_halt_timeout"], "3")==true) {
+            $logging->log("Successful soft power off by button...") ;
+            return true ; }
+        else {
+            $logging->log("Failed soft power off by button, attempting SSH shutdown.") ;
+
+            $sshParams = $this->params ;
+
+            $srv = array(
+                "user" => $this->papyrus["username"] ,
+                "password" => $this->papyrus["password"] ,
+                "target" => $this->papyrus["target"] );
+            $sshParams["yes"] = true ;
+            $sshParams["guess"] = true ;
+            $sshParams["servers"] = serialize(array($srv)) ;
+            $sshParams["ssh-data"] = $this->setSSHData("sudo shutdown now");
+
+            if (isset($this->phlagrantfile->config["ssh"]["port"])) {
+                $sshParams["port"] = $this->phlagrantfile->config["ssh"]["port"] ; }
+            if (isset($this->phlagrantfile->config["ssh"]["timeout"])) {
+                $sshParams["timeout"] = $this->phlagrantfile->config["ssh"]["timeout"] ; }
+            $sshFactory = new \Model\Invoke();
+            $ssh = $sshFactory->getModel($sshParams) ;
+            $ssh->performInvokeSSHData() ;
+
+            $logging->log("Attempting shutdown by SSH...") ;
+            $logging->log("Waiting up to {$this->phlagrantfile->config["vm"]["ssh_halt_timeout"]} seconds for machine to power off...") ;
+
+            if ($this->waitForStatus("poweredoff", $this->phlagrantfile->config["vm"]["ssh_halt_timeout"], "3")==true) {
+                $logging->log("Successful power off SSH Shutdown...") ;
+                return true ; } }
+        $lmsg = "Attempts to Halt this box by both Soft Power off and SSH Shutdown have failed. You may need to use ".
+            "phlagrant halt hard..." ;
+        $logging->log($lmsg) ;
+        return false ;
+
     }
 
     public function haltPause() {
@@ -38,6 +78,22 @@ class HaltAllLinux extends BaseLinuxApp {
         $this->loadFiles();
         $command = "vboxmanage controlvm {$this->phlagrantfile->config["vm"]["name"]} poweroff" ;
         $this->executeAndOutput($command);
+    }
+
+    # @todo in_array or something to check a sane status was requested
+    protected function waitForStatus($statusRequested, $total_time, $interval) {
+        for ($i=0; $i<$total_time; $i=$i+$interval) {
+            if($this->isVMInStatus($statusRequested)) {
+                return true ; }
+            sleep($interval); }
+        return false ;
+    }
+
+    protected function isVMInStatus($statusRequested) {
+        $command = "vboxmanage showvminfo \"{$this->phlagrantfile->config["vm"]["name"]}\" | grep \"State:\"  " ;
+        $out = $this->executeAndLoad($command);
+        $isStatusRequested = strpos($out, strtolower($statusRequested)) ;
+        return $isStatusRequested ;
     }
 
     protected function loadFiles() {
