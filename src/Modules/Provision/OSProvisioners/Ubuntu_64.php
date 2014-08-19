5dcda027c4fcc90746702ee0c6cb8a3878944791
David Amanshia <phpengine@hotmail.co.uk>
2014-08-19 02:11:54 +0100

    added support for shell and cleo to buntu provisioner

--- src/Modules/Provision/OSProvisioners/Ubuntu_64.php
+++ src/Modules/Provision/OSProvisioners/Ubuntu_64.php
@@ -6,13 +6,26 @@ class OSProvisioner extends ProvisionDefaultLinux {
 
     public $ostype = "Ubuntu 64 or 32 Bit from 10.04 onwards" ;
 
-    protected function setSSHData($provisionFile) {
+    public function getCleopatraInitSSHData($provisionFile) {
         $sshData = <<<"SSHDATA"
 echo {$this->phlagrantfile->config["ssh"]["password"]} | sudo -S apt-get update -y
 echo {$this->phlagrantfile->config["ssh"]["password"]} | sudo -S apt-get install -y php5 git
 git clone http://git.pharoah-tools.org.uk/git/phpengine/cleopatra.git
 echo {$this->phlagrantfile->config["ssh"]["password"]} | sudo -S php cleopatra/install-silent
+SSHDATA;
+        return $sshData ;
+    }
+
+    public function getStandardCleopatraSSHData($provisionFile) {
+        $sshData = <<<"SSHDATA"
 echo {$this->phlagrantfile->config["ssh"]["password"]} | sudo -S cleopatra auto x --af=$provisionFile
+SSHDATA;
+        return $sshData ;
+    }
+
+    public function getStandardShellSSHData($provisionFile) {
+        $sshData = <<<"SSHDATA"
+echo {$this->phlagrantfile->config["ssh"]["password"]} | sudo -S sh $provisionFile
 SSHDATA;
         return $sshData ;
     }
