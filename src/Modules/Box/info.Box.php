beb46e2a2b0453cc877c88bf5392d7c852e06eac
David Amanshia <phpengine@hotmail.co.uk>
2014-08-24 03:51:29 +0100

    added home location

--- src/Modules/Box/info.Box.php
+++ src/Modules/Box/info.Box.php
@@ -13,7 +13,7 @@ class BoxInfo extends Base {
     }
 
     public function routesAvailable() {
-        return array( "Box" =>  array_merge(array("add", "remove", "package", "list") ) );
+        return array( "Box" =>  array_merge(array("help", "add", "remove", "package", "list") ) );
     }
 
     public function routeAliases() {
@@ -33,6 +33,18 @@ class BoxInfo extends Base {
         - remove
         Removes the box as usable by Phlagrant
         example: phlagrant box remove
+
+        - package
+        Packages a box as usable by Phlagrant
+        example: phlagrant box package
+        example phlagrant box package --yes --guess
+            --name="Vanilla Ubuntu 12.04 amd 64"
+            --vmname="a4dc638f-2721-40c4-a943-2f2565c83fee" # use name or id of virtual machine
+            --provider="virtualbox" # guess will use virtualbox
+            --group="phlagrant"
+            --slug="" # guess can generate this based on name field
+            --description="A Vanilla install of Ubuntu..."
+            --home_location="http://www.someplace.net/" # guess will set this to http://www.phlagrant-box.org/
 
         - list
         List boxes installed in Phlagrant
