c607485d2782555e2bdd07f95f403e4e46f07083
David Amanshia <phpengine@hotmail.co.uk>
2014-08-27 17:28:55 +0100

    provision hook support

--- src/Modules/Provision/Model/ProvisionDefaultLinux.php
+++ src/Modules/Provision/Model/ProvisionDefaultLinux.php
@@ -8,26 +8,86 @@ class ProvisionDefaultLinux extends Base {
     public $papyrus ;
     protected $provisionModel ;
 
-    // @todo this should support other provisioners than pharoah, provide some override here to allow
-    // @todo chef solo, puppet agent, salt or ansible to get invoked
     public function provision($hook = "") {
         $provisionOuts = array() ;
-        $pharoahSpellings = array("Pharaoh", "pharaoh", "PharaohTools", "pharaohTools", "Pharoah", "pharoah", "PharoahTools", "pharoahTools") ;
         if ($hook != "") {$hook = "_$hook" ; }
         foreach ($this->phlagrantfile->config["vm"]["provision$hook"] as $provisionerSettings) {
-            if (in_array($provisionerSettings["provisioner"], array("shell", "bash", "Shell", "Bash"))) {
-                $provisionObjectFactory = new \Model\Shell() ;
-                $provisionObject = $provisionObjectFactory->getModel($this->params, "Provision");
-                $provisionObject->phlagrantfile = $this->phlagrantfile;
-                $provisionObject->papyrus = $this->papyrus;
-                $provisionOuts[] = $provisionObject->provision($provisionerSettings, $this) ; }
-            if (in_array($provisionerSettings["provisioner"], $pharoahSpellings)) {
-                $provisionObjectFactory = new \Model\PharaohTools() ;
-                $provisionObject = $provisionObjectFactory->getModel($this->params, "Provision");
-                $provisionObject->phlagrantfile = $this->phlagrantfile;
-                $provisionObject->papyrus = $this->papyrus;
-                $provisionOuts[] = $provisionObject->provision($provisionerSettings, $this) ; } }
+            $provisionOuts[] = $this->doSingleProvision($provisionerSettings) ; }
         return $provisionOuts ;
+    }
+
+    public function provisionHook($hook, $type) {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params) ;
+        $logging->log("Provisioning from Phlagrantfile settings if available for $hook $type") ;
+        $provisionOuts = $this->provisionPhlagrantfile($hook, $type) ;
+        $logging->log("Provisioning from hook directories if available for $hook $type") ;
+        $provisionOuts = array_merge($provisionOuts, $this->provisionHookDirs($hook, $type)) ;
+        return $provisionOuts ;
+    }
+
+    protected function provisionPhlagrantfile($hook, $type) {
+        $provisionOuts = array() ;
+        if (isset($this->phlagrantfile->config["vm"]["provision_{$hook}_{$type}"]) &&
+            count($this->phlagrantfile->config["vm"]["provision_{$hook}_{$type}"])>0){
+            foreach ($this->phlagrantfile->config["vm"]["provision_{$hook}_{$type}"] as $provisionerSettings) {
+                $provisionOuts[] = $this->doSingleProvision($provisionerSettings) ; } }
+        return $provisionOuts ;
+    }
+
+    protected function provisionHookDirs($hook, $type) {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params) ;
+        $provisionOuts = array() ;
+        // @todo this will do for now but should be dynamic
+        $provisioners = array("PharaohTools", "Shell") ;
+        foreach ($provisioners as $provisioner) {
+            // echo "dave a2\n" ;
+            // @todo this will do for now but should be dynamic
+            $tools = array("cleopatra", "dapperstrano") ;
+            foreach ($tools as $tool) {
+                // echo "dave a3\n" ;
+                $targets = array("host", "guest") ;
+                foreach ($targets as $target) {
+                    $dir = getcwd()."/build/config/phlagrant/hooks/$provisioner/$tool/$hook/$target/$type" ;
+                    $hookDirectoryExists = file_exists($dir) ;
+                    $hookDirectoryIsDir = is_dir($dir) ;
+                    // var_dump("hde", $hookDirectoryExists, "hdd", $hookDirectoryIsDir) ;
+                    // echo "dave a4 $dir\n" ;
+                    if ($hookDirectoryExists) {
+                        $relDir = str_replace(getcwd(), "", $dir) ;
+                        $logging->log("Phlagrant hook directory $relDir found") ;
+                        $hookDirFiles = scandir($dir) ;
+                        // echo "dave a5 x ".implode(" ", $hookDirFiles) ;
+                        foreach ($hookDirFiles as $hookDirFile) {
+                            // echo "dave a6\n" ;
+                            if (substr($hookDirFile, strlen($hookDirFile)-4) == ".php") {
+                                $logging->log("Phlagrant hook file $dir/$hookDirFile found") ;
+                                $provisionerSettings =
+                                    array(
+                                        "provisioner" => $provisioner,
+                                        "tool" => $tool,
+                                        "target" => $target,
+                                        "script" => "$dir/$hookDirFile"
+                                    );
+                                $provisionOuts[] = $this->doSingleProvision($provisionerSettings) ;
+                                $logging->log("Executing $hookDirFile with $tool") ; } } } } } }
+        return $provisionOuts ;
+    }
+
+
+    // @todo this should support other provisioners than pharaoh, provide some override here to allow
+    // @todo chef solo, puppet agent, salt or ansible to get invoked
+    protected function doSingleProvision($provisionerSettings) {
+        $pharaohSpellings = array("Pharaoh", "pharaoh", "PharaohTools", "pharaohTools", "Pharaoh", "pharaoh", "PharaohTools", "pharaohTools") ;
+        if (in_array($provisionerSettings["provisioner"], $pharaohSpellings)) {
+            $provisionObjectFactory = new \Model\PharaohTools() ; }
+        else if (in_array($provisionerSettings["provisioner"], array("shell", "bash", "Shell", "Bash"))) {
+            $provisionObjectFactory = new \Model\Shell() ; }
+        $provisionObject = $provisionObjectFactory->getModel($this->params, "Provision");
+        $provisionObject->phlagrantfile = $this->phlagrantfile;
+        $provisionObject->papyrus = $this->papyrus;
+        return $provisionObject->provision($provisionerSettings, $this) ;
     }
 
 }
