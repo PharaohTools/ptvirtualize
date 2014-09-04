686b93773dec1098d711d70de69f8da2bd982a03
David Amanshia <phpengine@hotmail.co.uk>
2014-09-04 11:32:00 +0100

    find base box runs again if downloaded a new one

--- src/Modules/Up/Model/UpImportBaseBoxAllLinux.php
+++ src/Modules/Up/Model/UpImportBaseBoxAllLinux.php
@@ -21,14 +21,45 @@ class UpImportBaseBoxAllLinux extends BaseLinuxApp {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params) ;
         $logging->log("Importing Base Box...") ;
-
-        // finding base box $this->phlagrantfile->config["vm"]["name"]} from Phlagrantfile
-        // find ova file in there
-        // run import command to  $this->phlagrantfile->config["vm"]["name"]}
-
         $baseBoxPath = $this->findBaseBox();
+        // if its null, we don't have the box yet, so box add it
+        if (is_null($baseBoxPath)) {
+            $logging->log("Base Box {$this->phlagrantfile->config["vm"]["box"]} doesn't exist locally, adding...") ;
+            // @lol simpsons below
+            $boxFactory = new \Model\Box();
+            $boxParams = $this->params ;
+            $boxParams["source"] = $this->getRemoteSource() ;
+            $boxParams["guess"] = true ; // guess target
+            if (strpos($this->phlagrantfile->config["vm"]["box"], "/") != false) {
+                $name = substr($this->phlagrantfile->config["vm"]["box"], strpos($this->phlagrantfile->config["vm"]["box"], "/")) ;
+                $logging->log("Guessing name $name ...") ; }
+            else {
+                $name = substr($this->phlagrantfile->config["vm"]["box"], strpos($this->phlagrantfile->config["vm"]["box"], "/")) ;
+                $logging->log("Guessing name $name ...") ; }
+            $boxParams["name"] = $name ;
+            $box = $boxFactory->getModel($boxParams) ;
+            $box->performBoxAdd() ;
+            $baseBoxPath = $this->findBaseBox(); }
         $ovaFile = $this->findOVAFile($baseBoxPath) ;
         $out = $this->doImport($ovaFile) ;
+        return $out ;
+    }
+
+    protected function getRemoteSource() {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params) ;
+        $home_url = "http://www.phlagrantboxes.co.uk/" ;
+        if (isset($this->phlagrantfile->config["vm"]["box_url"])) {
+            $source = $this->phlagrantfile->config["vm"]["box_url"] ;
+            $logging->log("Using explicit Box URL {$this->phlagrantfile->config["vm"]["box_url"]} from Phlagrantfile...") ; }
+        else if (strpos($this->phlagrantfile->config["vm"]["box"], "/") != false) {
+            $source = $home_url.$this->phlagrantfile->config["vm"]["box"] ;
+            $logging->log("Guessing Box URL {$home_url}{$this->phlagrantfile->config["vm"]["box"]} ...") ; }
+        else {
+            $source = $home_url.'phlagrant/'.$this->phlagrantfile->config["vm"]["box"] ;
+            // @todo dont DS this its a URL
+            $logging->log("Guessing Box URL {$home_url}phlagrant/{$this->phlagrantfile->config["vm"]["box"]} ...") ; }
+        return $source ;
     }
 
     protected function findBaseBox() {
@@ -63,6 +94,7 @@ class UpImportBaseBoxAllLinux extends BaseLinuxApp {
     protected function doImport($ovaFile) {
         $command  = "vboxmanage import {$ovaFile} --vsys 0 --ostype {$this->phlagrantfile->config["vm"]["ostype"]}" ;
         $command .= " --vmname {$this->phlagrantfile->config["vm"]["name"]}" ;
+        echo $command . "\n" ;
         $this->executeAndOutput($command);
         return true ;
     }
