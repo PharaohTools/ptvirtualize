d23692dc228b0f251eb9bc1a2e7d8ed7b23132f7
David Amanshia <phpengine@hotmail.co.uk>
2014-10-27 02:30:24 +0000

    fixed code to generate class from filename

--- src/Modules/PhlagrantRequired/Model/PhlagrantFileLoaderAllLinux.php
+++ src/Modules/PhlagrantRequired/Model/PhlagrantFileLoaderAllLinux.php
@@ -24,15 +24,15 @@ class PhlagrantFileLoaderAllLinux extends BaseLinuxApp {
         $phlagrantfilename = $this->getPfile() ;
         $cname = (isset($this->params["classname"])) ?
             $this->params["classname"] :
-            substr($phlagrantfilename, 0, strlen($phlagrantfilename)-4) ;
+            str_replace(".php", "", $phlagrantfilename) ;
         $cname = '\Model\\'.$cname  ;
         $phlagrant = (class_exists($cname)) ? new $cname($this->params) : null ;
         return $phlagrant;
     }
 
     private function getPfile() {
-        $phlagrantfile = null ;
-        $phlagrantfile = (isset($this->params["pfile"])) ? $this->params["pfile"] : null ;
+        $phlagrantfile = "Phlagrantfile" ;
+        $phlagrantfile = (isset($this->params["pfile"])) ? $this->params["pfile"] : $phlagrantfile ;
         $phlagrantfile = (isset($this->params["phlagrantfile"])) ? $this->params["phlagrantfile"] : $phlagrantfile ;
         return $phlagrantfile;
     }
