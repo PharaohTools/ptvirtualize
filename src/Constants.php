841444fe2509b1c612a4fae783f4869ad33cc310
David Amanshia <phpengine@hotmail.co.uk>
2014-11-10 19:12:52 +0000

    fixed boxdir trail slash

--- src/Constants.php
+++ src/Constants.php
@@ -14,7 +14,7 @@ if (in_array(PHP_OS, array("Windows", "WINNT"))) {
     define('CLEOCOMM', "cleopatra.bat") ;
     define('DAPPCOMM', "dapperstrano.bat") ;
     define('PHLCOMM', "phlagrant.bat") ;
-    define('BOXDIR', $pf.'phlagrant\boxes'.'\\') ;
+    define('BOXDIR', $pf.'\\'.'phlagrant\boxes') ;
     define('DS', "\\");
     define('BASE_TEMP_DIR', 'C:\\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
