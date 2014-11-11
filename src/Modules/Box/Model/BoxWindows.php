4fc31a065fcde2981f9c539a1ea354136a1287cf
David Amanshia <phpengine@hotmail.co.uk>
2014-11-11 01:27:00 +0000

    debugs

--- src/Modules/Box/Model/BoxWindows.php
+++ src/Modules/Box/Model/BoxWindows.php
@@ -37,7 +37,7 @@ class BoxWindows extends BoxUbuntu {
     }
 
     protected function extractMetadata() {
-        $pd = new \Phar($this->source) ;
+        $pd = new \Phar(realpath($this->source), 0, "boxphar") ;
         var_dump($pd) ;
         //$pd->extractTo(BASE_TEMP_DIR, "metadata.json", true) ;
         //$fData = file_get_contents(BASE_TEMP_DIR."metadata.json") ;
