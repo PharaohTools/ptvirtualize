cab4843c4389b549416d3181ef8280e0f6405705
David Amanshia <phpengine@hotmail.co.uk>
2014-11-11 01:39:17 +0000

    fixed gnu tar path

--- src/Modules/Box/Model/BoxWindows.php
+++ src/Modules/Box/Model/BoxWindows.php
@@ -38,7 +38,7 @@ class BoxWindows extends BoxUbuntu {
 
     protected function extractMetadata() {
         $boxFile = $this->source ;
-        $tarExe = '"'.dirname(dirname(dirname(__FILE__))).'\Tar\Packages\TarGnu\Tar.exe"' ;
+        $tarExe = '"'.dirname(dirname(dirname(__FILE__))).'\Tar\Packages\TarGnu\bin\Tar.exe"' ;
         $command = "$tarExe --extract --file=$boxFile -C ".BASE_TEMP_DIR." .".DS."metadata.json" ;
         self::executeAndOutput($command);
         $fData = file_get_contents(BASE_TEMP_DIR."metadata.json") ;
