e042319bc0ca747b3a3a91d3d73e4b656a15664c
David Amanshia <phpengine@hotmail.co.uk>
2014-11-11 03:20:23 +0000

    NEW BOX DIR

--- src/Constants.php
+++ src/Constants.php
@@ -14,7 +14,7 @@ if (in_array(PHP_OS, array("Windows", "WINNT"))) {
     define('CLEOCOMM', "cleopatra.bat") ;
     define('DAPPCOMM', "dapperstrano.bat") ;
     define('PHLCOMM', "phlagrant.bat") ;
-    define('BOXDIR', $pf.'C:\\PharaohTools\boxes') ;
+    define('BOXDIR', 'C:\\PharaohTools\boxes') ;
     define('DS', "\\");
     define('BASE_TEMP_DIR', 'C:\\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
