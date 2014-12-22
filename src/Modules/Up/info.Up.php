b5a758422e1617a7feae7c7be99c37363b47dc77
David Amanshia <phpengine@hotmail.co.uk>
2014-12-22 02:50:11 +0000

    more verbose help for up

--- src/Modules/Up/info.Up.php
+++ src/Modules/Up/info.Up.php
@@ -22,13 +22,20 @@ class UpInfo extends CleopatraBase {
 
   public function helpDefinition() {
     $help = <<<"HELPDATA"
-  This command allows you to create, start and provision phlagrant boxes
+  This command allows you to create, start and provision phlagrant boxes.
 
   Up, up
 
         - now
         Bring up a box now
-        example: phlagrant up now --provision
+        example: phlagrant up now
+        example: phlagrant up now --modify # modify the hardware settings to match the Phlagrantfile during the up phase.
+            Without it, the machine will be brought up with its previous settings. On creating new machines this will
+            happen automatically regardless of the parameter.
+        example: phlagrant up now --provision # provision an existing machine with the configuration scripts specified
+            in the Phlagrantfile. Without it, the machine will be brought up with its previous config. On creating
+            new machines this will happen automatically regardless of the parameter.
+        example: phlagrant up now --modify --provision # modify and provision an existing box during the up phase
 
 HELPDATA;
     return $help ;
