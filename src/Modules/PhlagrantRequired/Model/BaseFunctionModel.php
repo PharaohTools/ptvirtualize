3981ea540b1de8489b1378fb7ddb306e49861239
David Amanshia <phpengine@hotmail.co.uk>
2014-12-27 04:14:04 +0000

    fixed findprovide to use phlagrant file

--- src/Modules/PhlagrantRequired/Model/BaseFunctionModel.php
+++ src/Modules/PhlagrantRequired/Model/BaseFunctionModel.php
@@ -1,8 +1,68 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: golden
- * Date: 27/12/14
- * Time: 03:55
- * To change this template use File | Settings | File Templates.
- */
+
+Namespace Model;
+
+class BaseFunctionModel extends BaseLinuxApp {
+
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("Default") ;
+
+    protected $phlagrantfile;
+    protected $papyrus ;
+    protected $provider ;
+
+    public function __construct($params) {
+        parent::__construct($params);
+        $this->initialize();
+    }
+
+    protected function loadFiles() {
+        $this->phlagrantfile = $this->loadPhlagrantFile();
+        $this->papyrus = $this->loadPapyrusLocal();
+    }
+
+    protected function loadPhlagrantFile() {
+        $prFactory = new \Model\PhlagrantRequired();
+        $phlagrantFileLoader = $prFactory->getModel($this->params, "PhlagrantFileLoader") ;
+        return $phlagrantFileLoader->load() ;
+    }
+
+    protected function loadPapyrusLocal() {
+        $prFactory = new \Model\PhlagrantRequired();
+        $papyrusLocalLoader = $prFactory->getModel($this->params, "PapyrusLocalLoader") ;
+        return $papyrusLocalLoader->load($this->phlagrantfile) ;
+    }
+
+    protected function findProvider($modGroup = "BoxDestroy") {
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params) ;
+        if (isset($this->phlagrantfile->config["vm"]["provider"])) {
+            $logging->log("Provider {$this->phlagrantfile->config["vm"]["provider"]} found in Phlagrantfile") ;
+            $this->provider = $this->getProvider($this->phlagrantfile->config["vm"]["provider"], $modGroup) ; }
+        else {
+            $logging->log("No Provider configured in Phlagrantfile."); }
+    }
+
+    protected function getProvider($provider, $modGroup) {
+        $infoObjects = \Core\AutoLoader::getInfoObjects();
+        $allProviders = array();
+        foreach($infoObjects as $infoObject) {
+            if ( method_exists($infoObject, "vmProviderName") ) {
+                $allProviders[] = $infoObject->vmProviderName(); } }
+        foreach($allProviders as $oneProvider) {
+            if ( $provider == $oneProvider ) {
+                $className = '\Model\\'.ucfirst($oneProvider) ;
+                $providerFactory = new $className();
+                $provider = $providerFactory->getModel($this->params, $modGroup);
+                return $provider ; } }
+        return false ;
+    }
+
+}
