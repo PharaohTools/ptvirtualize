f3c82bf88a4476662a2cc5fd638f8583b29ab0b1
David Amanshia <phpengine@hotmail.co.uk>
2014-12-27 04:32:51 +0000

    updated status module to use provider only functionality

--- src/Modules/Status/Model/StatusAllOS.php
+++ src/Modules/Status/Model/StatusAllOS.php
@@ -2,7 +2,7 @@
 
 Namespace Model;
 
-class StatusAllOS extends BaseLinuxApp {
+class StatusAllOS extends BaseFunctionModel {
 
     // Compatibility
     public $os = array("any") ;
@@ -14,9 +14,6 @@ class StatusAllOS extends BaseLinuxApp {
     // Model Group
     public $modelGroup = array("Default") ;
 
-    protected $phlagrantfile;
-    protected $papyrus ;
-
     public function __construct($params) {
         parent::__construct($params);
         $this->initialize();
@@ -24,37 +21,14 @@ class StatusAllOS extends BaseLinuxApp {
 
     public function statusShow() {
         $this->loadFiles();
-        $command = VBOXMGCOMM." showvminfo \"{$this->phlagrantfile->config["vm"]["name"]}\"  " ;
-        $out = $this->executeAndLoad($command);
-        $outLines = explode("\n", $out);
-        foreach ($outLines as $outLine) {
-            if (strpos($outLine, "State:") !== false) {
-                return $outLine."\n" ; } }
-        return null ;
+        $this->findProvider("BoxStatus");
+        return $this->provider->statusShow($this->phlagrantfile->config["vm"]["name"]);
     }
 
     public function statusFull() {
         $this->loadFiles();
-        $command = VBOXMGCOMM." showvminfo \"{$this->phlagrantfile->config["vm"]["name"]}\" " ;
-        $status = $this->executeAndLoad($command) ;
-        return $status ;
-    }
-
-    protected function loadFiles() {
-        $this->phlagrantfile = $this->loadPhlagrantFile();
-        $this->papyrus = $this->loadPapyrusLocal();
-    }
-
-    protected function loadPhlagrantFile() {
-        $prFactory = new \Model\PhlagrantRequired();
-        $phlagrantFileLoader = $prFactory->getModel($this->params, "PhlagrantFileLoader") ;
-        return $phlagrantFileLoader->load() ;
-    }
-
-    protected function loadPapyrusLocal() {
-        $prFactory = new \Model\PhlagrantRequired();
-        $papyrusLocalLoader = $prFactory->getModel($this->params, "PapyrusLocalLoader") ;
-        return $papyrusLocalLoader->load($this->phlagrantfile) ;
+        $this->findProvider("BoxStatus");
+        return $this->provider->statusFull($this->phlagrantfile->config["vm"]["name"]);
     }
 
 }
