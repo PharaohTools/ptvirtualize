6ca9a7d937e4543b0dbca3acbc4c014449f84f4b
David Amanshia <phpengine@hotmail.co.uk>
2014-12-27 04:11:59 +0000

    base function model for shared methods in command models, and box halt for vb

---
+++ src/Modules/PhlagrantRequired/Model/BaseFunctionModel.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: golden
+ * Date: 27/12/14
+ * Time: 03:55
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ src/Modules/Virtualbox/Model/BoxHalt.php
@@ -0,0 +1,27 @@
+<?php
+
+Namespace Model;
+
+class BoxDestroy extends BaseVirtualboxAllOS {
+
+    // Compatibility
+    public $os = array("any") ;
+    public $linuxType = array("any") ;
+    public $distros = array("any") ;
+    public $versions = array("any") ;
+    public $architectures = array("any") ;
+
+    // Model Group
+    public $modelGroup = array("BoxDestroy") ;
+
+    public function destroyVM($name) {
+        $this->provider->destroyVM();
+        $command = VBOXMGCOMM." unregistervm {$name} --delete" ;
+        $this->executeAndOutput($command);
+    }
+
+    public function getDestroyableStates() {
+        return array("aborted", "powered off") ;
+    }
+
+}
