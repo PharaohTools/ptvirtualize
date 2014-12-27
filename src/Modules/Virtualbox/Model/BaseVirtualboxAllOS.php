999a9003b66f1af0be3da5ef1ef382e3b4a53c2a
David Amanshia <phpengine@hotmail.co.uk>
2014-12-27 03:50:44 +0000

    base virtualbox provides is in status method

--- src/Modules/Virtualbox/Model/BaseVirtualboxAllOS.php
+++ src/Modules/Virtualbox/Model/BaseVirtualboxAllOS.php
@@ -14,4 +14,20 @@ class BaseVirtualboxAllOS extends Base {
     // Model Group
     public $modelGroup = array("Base") ;
 
+    public function isVMInStatus($vm, $statusRequested) {
+        $command = VBOXMGCOMM." showvminfo \"{$vm}\" " ;
+        $out = $this->executeAndLoad($command);
+        $outLines = explode("\n", $out);
+        $outStr = "" ;
+        foreach ($outLines as $outLine) {
+            if (strpos($outLine, "State:") !== false) {
+                $outStr .= $outLine."\n" ;
+                break; } }
+        if (!is_array($statusRequested)) {$statusRequested = array($statusRequested);}
+        foreach ($statusRequested as $stat) {
+            if (strpos($outStr, strtolower($stat))==true) {
+                return true ; } }
+        return false ;
+    }
+
 }
