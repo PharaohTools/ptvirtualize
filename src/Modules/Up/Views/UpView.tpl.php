24bca13bbfc108b4e6459c1f1fc9cff25ac9c70b
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-27 15:41:19 +0000

    up shows success or failure steps

--- src/Modules/Up/Views/UpView.tpl.php
+++ src/Modules/Up/Views/UpView.tpl.php
@@ -1,6 +1,25 @@
 <?php
 
-   echo $pageVars["result"] ;
+foreach ($pageVars["result"] as $key => $value) {
+    ?>
+    Step <?php echo $key ; ?> : <?php echo ($value==true) ? "Success" : "Failure" ; ?>/
+<?php
+}
+
+$success = !in_array(false, $pageVars["result"]) ;
+
+if ($success==true) { ?>
+Up Successful
+
+<?php
+}
+
+else { ?>
+Up Failed
+
+<?php
+}
+
 ?>
 
 In Virtualize Up
