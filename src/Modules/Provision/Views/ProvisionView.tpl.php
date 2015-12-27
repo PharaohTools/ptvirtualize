55ec7ba98cfdec491e7570f945950ed63ae6e923
Pharaoh Tools <phpengine@hotmail.co.uk>
2015-12-27 15:38:37 +0000

    provision view shows success or failure per step

--- src/Modules/Provision/Views/ProvisionView.tpl.php
+++ src/Modules/Provision/Views/ProvisionView.tpl.php
@@ -1,18 +1,23 @@
 <?php
 
+
+foreach ($pageVars["result"] as $key => $value) {
+
+?>
+Step <?php echo $key ; ?> : <?php echo ($value==true) ? "Success" : "Failure" ; ?>/
+<?php
+}
+
 $success = !in_array(false, $pageVars["result"]) ;
 
 if ($success==true) { ?>
-
-    Provision Successful
+Provision Successful
 
 <?php
 }
 
 else { ?>
-
-    Provision Failed
-
+Provision Failed
 
 <?php
 }
