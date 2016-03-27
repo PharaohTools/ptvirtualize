dcb544eec4549ec1139661be9ad65f416aaf4d90
Pharaoh Tools <phpengine@hotmail.co.uk>
2016-03-27 17:42:57 +0100

    handling false returns from controller

--- src/Modules/Provision/Views/ProvisionView.tpl.php
+++ src/Modules/Provision/Views/ProvisionView.tpl.php
@@ -1,9 +1,11 @@
 <?php
 
-foreach ($pageVars["result"] as $key => $value) {
-    ?>Step <?php echo $key ; ?> : <?php echo ($value==true) ? "Success\n" : "Failure\n" ; }
-
-$success = !in_array(false, $pageVars["result"]) ;
+if ($pageVars["result"] === false) {
+    $success = false ; }
+else {
+    foreach ($pageVars["result"] as $key => $value) {
+        ?>Step <?php echo $key ; ?> : <?php echo ($value==true) ? "Success\n" : "Failure\n" ; }
+    $success = !in_array(false, $pageVars["result"]) ; }
 
 if ($success==true) { ?>
 - Provision Successful
