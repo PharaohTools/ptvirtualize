9536d0e34aca4c673639dd6648db761830b42a33
David Amanshia <phpengine@pharaohtools.com>
2016-10-09 08:03:44 +0100

    updated constants to match newest configure

--- src/Constants.php
+++ src/Constants.php
@@ -4,9 +4,10 @@
  * Pharaoh Tools Constants
  */
 
-define('PHARAOH_APP', "ptvirtualize") ;
+define('PHARAOH_APP', "ptconfigure") ;
 
 if (in_array(PHP_OS, array("Windows", "WINNT"))) {
+    $sd = getenv('SystemDrive') ;
     $pf = getenv('ProgramFiles') ;
     $pf = str_replace(" (x86)", "", $pf) ;
     $command = "where /R \"{$pf}\" *VBoxManage* " ;
@@ -14,24 +15,41 @@ if (in_array(PHP_OS, array("Windows", "WINNT"))) {
     exec($command, $outputArray);
     define('SUDOPREFIX', "");
     define('VBOXMGCOMM', "\"{$outputArray[0]}\" ") ;
-    define('PTCCOMM', '"C:\PharaohTools\ptconfigure.cmd"') ;
-    define('PTDCOMM', '"C:\PharaohTools\ptdeploy.cmd"') ;
-    define('PTVCOMM', '"C:\PharaohTools\ptvirtualize.cmd"') ;
-    define('BOXDIR', 'C:\\PharaohTools\boxes') ;
-    define('DS', "\\");
-    define('BASE_TEMP_DIR', 'C:\\Temp\\'); }
+    define('PFILESDIR', $sd."\\PharaohTools\\") ;
+    define('PTCCOMM', PFILESDIR.'ptconfigure.cmd"') ;
+    define('PTBCOMM', PFILESDIR.'ptbuild.cmd"') ;
+    define('PTDCOMM',  PFILESDIR."ptdeploy.cmd") ;
+    define('PTVCOMM',  PFILESDIR."ptvirtualize.cmd") ;
+    define('PTTRCOMM',  PFILESDIR."pttrack.cmd") ;
+    define('PTTECOMM',  PFILESDIR."pttest.cmd") ;
+    define('PTSCOMM',  PFILESDIR."ptsource.cmd") ;
+    define('PTMCOMM',  PFILESDIR."ptmanage.cmd") ;
+    define('BOXDIR', $sd.'\\PharaohTools\boxes') ;
+    define('PIPEDIR', $sd.'\\PharaohTools\pipes'.'\\') ;
+    define('REPODIR', $sd.'\\PharaohTools\repositories'.'\\') ;
+    define("DS", "\\");
+    define("BASE_TEMP_DIR", getenv("SystemDrive").'\Temp\\'); }
 else if (in_array(PHP_OS, array("Linux", "Solaris", "FreeBSD", "OpenBSD", "Darwin"))) {
     $uname = exec('whoami');
     $isAdmin = ($uname == "root") ? true : false ;
     if ($isAdmin == true) { define('SUDOPREFIX', ""); }
     else { define('SUDOPREFIX', "sudo "); }
     define('VBOXMGCOMM', "vboxmanage ") ;
+    define('PFILESDIR', "/opt/") ;
     define('PTCCOMM', "ptconfigure ") ;
+    define('PTBCOMM', "ptbuild ") ;
     define('PTDCOMM', "ptdeploy ") ;
     define('PTVCOMM', "ptvirtualize") ;
-    define('BOXDIR', '/opt/ptvirtualize/boxes') ;
-    define('DS', "/");
-    define('BASE_TEMP_DIR', '/tmp/'); }
+    define('PTTRCOMM', "pttrack") ;
+    define('PTTECOMM', "pttest") ;
+    define('PTMCOMM',  "ptmanage") ;
+    define('PTSCOMM', "ptsource") ;
+    define("DS", "/");
+    define("BASE_TEMP_DIR", '/tmp/');
+    define('BOXDIR', '/ptvirtualize/boxes'.'\\') ;
+    define('PIPEDIR', '/opt/ptbuild/pipes') ;
+    define('REPODIR', '/opt/ptsource/repositories') ; }
 
 // LOG LEVELS
 define('LOG_FAILURE_EXIT_CODE', 1) ;
+define('APPLICATION_LOG', '/var/log/pharaoh.log') ;
