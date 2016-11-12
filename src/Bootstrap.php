f5f86c482c6a774c9803261d1a764044e47f3d9e
David Amanshia <phpengine@pharaohtools.com>
2016-11-12 05:57:45 +0000

    bootstrap typo

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once ("BootstrapCore.php") ;
 
-$bootStrap = new BootStrap();
+$bootStrap = new \Core\BootStrap();
 $argv_or_null = (isset($argv)) ? $argv : null ;
 $bootStrapParams = (isset($_ENV['ptconfigure_bootstrap'])) ? unserialize($_ENV['ptconfigure_bootstrap']) : $argv_or_null ;
 $bootStrap->setExitCode(0) ;
