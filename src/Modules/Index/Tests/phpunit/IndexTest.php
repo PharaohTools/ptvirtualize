aa200cc195a1f16564b0a715524e70876c7cd2f1
David Amanshia <phpengine@pharaohtools.com>
2016-11-13 05:07:52 +0000

    index test updated to run

--- src/Modules/Index/Tests/phpunit/IndexTest.php
+++ src/Modules/Index/Tests/phpunit/IndexTest.php
@@ -3,17 +3,36 @@
 class IndexTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
-        require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR."Bootstrap.php");
+        $bd = dirname(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR ;
+        try {
+            require_once ($bd.'AutoLoad.php') ;
+            $autoLoader = new \Core\autoLoader();
+            $autoLoader->launch(); }
+        catch (\Exception $e) {
+            echo "Setup cant load autoloader\n" ;
+            echo 'Message: ' .$e->getMessage(); }
+        try {
+            require_once ($bd.'Constants.php') ; }
+        catch (\Exception $e) {
+            echo "Setup cant load constants\n" ;
+            echo 'Message: ' .$e->getMessage(); }
+        try {
+            require_once ($bd.'BootstrapCore.php') ; }
+        catch (\Exception $e) {
+            echo "Setup cant load Bootstrap Core Class\n" ;
+            echo 'Message: ' .$e->getMessage(); }
     }
 
     public function testExecuteReturnsAValue() {
         $controlObject = new \Controller\Index();
-        $this->assertTrue ( $controlObject->execute( array() ) != null );
+        $res = $this->assertTrue ( $controlObject->execute( array() ) != null );
+        return $res ;
     }
 
     public function testExecuteReturnsAValueOfTypeArray() {
         $controlObject = new \Controller\Index();
-        $this->assertTrue ( is_array($controlObject->execute( array() ) ) );
+        $res = $this->assertTrue ( is_array($controlObject->execute( array() ) ) );
+        return $res ;
     }
 
 }
