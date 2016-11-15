0f3df4b05e0a5fe73d6e1111698c0a6fe8bdaca5
David Amanshia <phpengine@pharaohtools.com>
2016-11-15 18:36:01 +0000

    index test works, provides enough for controller

--- src/Modules/Index/Tests/phpunit/IndexTest.php
+++ src/Modules/Index/Tests/phpunit/IndexTest.php
@@ -4,36 +4,48 @@ class IndexTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
         $bd = dirname(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR ;
-//        try {
-//            require_once ($bd.'AutoLoad.php') ;
-//            $autoLoader = new \Core\autoLoader();
-//            $autoLoader->launch(); }
-//        catch (\Exception $e) {
-//            echo "Setup cant load autoloader\n" ;
-//            echo 'Message: ' .$e->getMessage(); }
-//        try {
-//            require_once ($bd.'Constants.php') ; }
-//        catch (\Exception $e) {
-//            echo "Setup cant load constants\n" ;
-//            echo 'Message: ' .$e->getMessage(); }
-//        try {
-//            require_once ($bd.'BootstrapCore.php') ; }
-//        catch (\Exception $e) {
-//            echo "Setup cant load Bootstrap Core Class\n" ;
-//            echo 'Message: ' .$e->getMessage(); }
+        try {
+            require_once ($bd.'AutoLoad.php') ;
+            $autoLoader = new \Core\autoLoader();
+            $autoLoader->launch(); }
+        catch (\Exception $e) {
+            echo "Setup cant load autoloader\n" ;
+            echo 'Message: ' .$e->getMessage(); }
+        try {
+            require_once ($bd.'Constants.php') ; }
+        catch (\Exception $e) {
+            echo "Setup cant load constants\n" ;
+            echo 'Message: ' .$e->getMessage(); }
+        try {
+            require_once ($bd.'BootstrapCore.php') ; }
+        catch (\Exception $e) {
+            echo "Setup cant load Bootstrap Core Class\n" ;
+            echo 'Message: ' .$e->getMessage(); }
     }
 
     public function testExecuteReturnsAValue() {
-//        $controlObject = new \Controller\Index();
-//        $res = $this->assertTrue ( $controlObject->execute( array() ) != null );
-        $res = true ;
+        $pv = array(
+            "route" => array(
+                "action" => "Index",
+                "control" => "index",
+                "extraParams" => array()
+            )
+        ) ;
+        $controlObject = new \Controller\Index();
+        $res = $this->assertTrue ( $controlObject->execute( $pv ) != null );
         return $res ;
     }
 
     public function testExecuteReturnsAValueOfTypeArray() {
-//        $controlObject = new \Controller\Index();
-//        $res = $this->assertTrue ( is_array($controlObject->execute( array() ) ) );
-        $res = true ;
+        $pv = array(
+            "route" => array(
+                "action" => "Index",
+                "control" => "index",
+                "extraParams" => array()
+            )
+        ) ;
+        $controlObject = new \Controller\Index();
+        $res = $this->assertTrue ( is_array($controlObject->execute( $pv ) ) );
         return $res ;
     }
 
