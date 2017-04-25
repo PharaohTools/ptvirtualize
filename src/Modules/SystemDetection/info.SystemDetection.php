070da059721dcb70e3c399afcf86a141224115dd
Pharaoh Tools <phpengine@pharaohtools.com>
2017-04-25 14:30:15 +0100

    efficient system detection

--- src/Modules/SystemDetection/info.SystemDetection.php
+++ src/Modules/SystemDetection/info.SystemDetection.php
@@ -2,7 +2,7 @@
 
 Namespace Info;
 
-class SystemDetectionInfo extends Base {
+class SystemDetectionInfo extends PTConfigureBase {
 
     public $hidden = false;
 
@@ -13,36 +13,26 @@ class SystemDetectionInfo extends Base {
     }
 
     public function routesAvailable() {
-      return array( "SystemDetection" =>  array_merge(array("detect", "help") ) );
+      return array( "SystemDetection" =>  array_merge(array("detect", 'generate-defaults', "help") ) );
     }
 
     public function routeAliases() {
       return array("system-detection"=>"SystemDetection", "systemdetection"=>"SystemDetection");
     }
 
-    public function autoPilotVariables() {
-      return array(
-        "SystemDetection" => array(
-          "SystemDetection" => array(
-            "programDataFolder" => "/etc/apache2/", // command and app dir name
-            "programNameMachine" => "systemdetection", // command and app dir name
-            "programNameFriendly" => "System Detect", // 12 chars
-            "programNameInstaller" => "System Detection",
-          )
-        )
-      );
-    }
-
     public function helpDefinition() {
       $help = <<<"HELPDATA"
-  This is a default Module and provides you with a method by which you can configure Application Settings.
-  You can configure default application settings, ie: mysql admin user, host, pass
+  This is a default Module and provides you with a method for accessing basic system values
 
   SystemDetection, system-detection, systemdetection
 
         - detect
         Detects the Operating System
-        example: ptconfigure system-detection detect
+        example: ptbuild system-detection detect
+
+        - generate-defaults
+        Generates a file of defaults for better performance
+        example: ptbuild system-detection generate-defaults
 
 HELPDATA;
       return $help ;
