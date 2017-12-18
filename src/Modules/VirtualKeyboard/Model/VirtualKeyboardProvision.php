46696d1358f07d8828deeecdbde5ddc9fa376ad7
David Amanshia <phpengine@pharaohtools.com>
2017-12-18 16:03:46 +0000

    keyboard provisioning fixes

--- src/Modules/VirtualKeyboard/Model/VirtualKeyboardProvision.php
+++ src/Modules/VirtualKeyboard/Model/VirtualKeyboardProvision.php
@@ -30,17 +30,40 @@ class VirtualKeyboardProvision extends BaseVirtualKeyboardAllOS {
             return false ; }
     }
 
-    protected function virtualKeyboardProvision($provisioner, $osProvisioner) {
+    protected function virtualKeyboardProvision($provisionerSettings, $osProvisioner) {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params);
         $logging->log("Provisioning Host with VirtualKeyboard...", $this->getModuleName());
-//        $command = "sh {$provisioner["script"]}" ;
-        $command = "VBoxManage guestcontrol openstackdevstack " ;
+//        $command = "sh {$provisionerSettings["script"]}" ;
+
+        if (isset($provisionerSettings["default"])) {
+            $logging->log("Attempting to use default shell script {$provisionerSettings["default"]}", $this->getModuleName());
+            $methodName = "get".ucfirst($provisionerSettings["default"])."SSHData" ;
+            if (method_exists($osProvisioner, $methodName)) {
+                $logging->log("Found {$provisionerSettings["default"]} method in OS Provisioner", $this->getModuleName());
+                $keyboard_data = $osProvisioner->$methodName() ; }
+            else {
+                $logging->log("No method {$provisionerSettings["default"]} found in OS Provisioner, cannot continue", $this->getModuleName(), LOG_FAILURE_EXIT_CODE);
+                return false ; } }
+        else {
+//            $logging->log("Attempting to use Standard shell script {$init["provision_file"]}", $this->getModuleName());
+//            $methodName = "getStandardShellSSHData" ;
+//            if (method_exists($osProvisioner, $methodName)) {
+//                $logging->log("Found {$methodName} method in OS Provisioner", $this->getModuleName());
+//                $sshParams["ssh-data"] = $osProvisioner->$methodName($init["provision_file"]) ; }
+//            else {
+//                $logging->log("No method {$methodName} found in OS Provisioner, cannot continue", $this->getModuleName(), LOG_FAILURE_EXIT_CODE);
+//                return false ; }
+        }
+
+        var_dump("Keyboard Data", $keyboard_data) ;
+
+        $command = "VBoxManage guestcontrol {$this->virtufile->config["vm"]["name"]} " ;
         $command .= "--username {$this->virtufile->config["ssh"]["user"]} " ;
         $command .= "--password {$this->virtufile->config["ssh"]["password"]} " ;
-        $command .= "run --exe {$provisioner["script"]} " ;
+        $command .= "run --exe {$keyboard_data} " ;
         $command .= "--wait-stdout --wait-stderr " ;
-        # VBoxManage guestcontrol openstackdevstack --username stack --password stack run --exe /opt/devstack/stack_wrap.sh --wait-stdout --wait-stderr
+
         $rc = self::executeAndOutput($command, null, true) ;
         return ($rc === 0) ? true : false ;
     }
