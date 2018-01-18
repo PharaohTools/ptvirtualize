678d9aae3b72ecfd51f8686e1fc2b73af8e945f3
David Amanshia <david.amanshia@sapient.com>
2018-01-18 09:58:35 +0000

    native downloading, same for all os

--- src/Modules/Box/Model/BoxLinuxMac.php
+++ src/Modules/Box/Model/BoxLinuxMac.php
@@ -22,6 +22,8 @@ class BoxLinuxMac extends BaseLinuxApp {
     protected $provider ;
     protected $metadata ;
 
+    protected $cur_progress ;
+
     public function __construct($params) {
         parent::__construct($params);
         $this->actionsToMethods = $this->setActionsToMethods() ;
@@ -264,28 +266,120 @@ class BoxLinuxMac extends BaseLinuxApp {
             return false ; }
     }
 
+//    protected function downloadIfRemote() {
+//        $loggingFactory = new \Model\Logging();
+//        $logging = $loggingFactory->getModel($this->params) ;
+//        if (substr($this->source, 0, 7) == "http://" || substr($this->source, 0, 8) == "https://") {
+//            $this->source = $this->ensureTrailingSlash($this->source);
+//            $logging->log("Box is remote not local, will download to temp directory before adding...", $this->getModuleName());
+//            set_time_limit(0); // unlimited max execution time
+//            $tmpFile = BASE_TEMP_DIR.'file.box' ;
+//            $logging->log("Downloading File ...", $this->getModuleName()) ;
+//            if (substr($this->source, strlen($this->source)-1, 1) == '/') {
+//                $this->source = substr($this->source, 0, strlen($this->source)-1) ; }
+//            // @todo error return false
+//            $curlComm = 'curl -L "'.$this->source.'" -o "'.$tmpFile.'" ' ;
+//            self::executeAndOutput($curlComm);
+//            // @todo this should be done with an rc but thers no friendly output fir some reason
+////            $rt = self::executeAndGetReturnCode($curlComm, true, true) ;
+////            if ($rt["rc"] !== 0) { return false;}
+//            $this->source = $tmpFile ;
+//            $logging->log("Download complete ...", $this->getModuleName());
+//            return true ;}
+//        return true ;
+//    }
+
     protected function downloadIfRemote() {
         $loggingFactory = new \Model\Logging();
         $logging = $loggingFactory->getModel($this->params) ;
         if (substr($this->source, 0, 7) == "http://" || substr($this->source, 0, 8) == "https://") {
-            $this->source = $this->ensureTrailingSlash($this->source);
-            $logging->log("Box is remote not local, will download to temp directory before adding...", $this->getModuleName());
+            $logging->log("Box is remote not local, will download to temp directory before adding...", $this->getModuleName()) ;
             set_time_limit(0); // unlimited max execution time
             $tmpFile = BASE_TEMP_DIR.'file.box' ;
-            $logging->log("Downloading File ...", $this->getModuleName()) ;
-            if (substr($this->source, strlen($this->source)-1, 1) == '/') {
-                $this->source = substr($this->source, 0, strlen($this->source)-1) ; }
-            // @todo error return false
-            $curlComm = 'curl -L "'.$this->source.'" -o "'.$tmpFile.'" ' ;
-            self::executeAndOutput($curlComm);
-            // @todo this should be done with an rc but thers no friendly output fir some reason
-//            $rt = self::executeAndGetReturnCode($curlComm, true, true) ;
-//            if ($rt["rc"] !== 0) { return false;}
-            $this->source = $tmpFile ;
-            $logging->log("Download complete ...", $this->getModuleName());
-            return true ;}
+            $res = $this->packageDownload($this->source, $tmpFile) ;
+            if ($res == true) {
+                $this->source = $tmpFile ;
+                $logging->log("Download complete ...", $this->getModuleName());
+                return true ;
+            } else {
+                $logging->log("File Download Failed", $this->getModuleName());
+                return false; } }
+        $logging->log("File Path is local not remote, no download required", $this->getModuleName());
         return true ;
     }
+
+    public function packageDownload($remote_source, $temp_exe_file) {
+        if (file_exists($temp_exe_file)) {
+            unlink($temp_exe_file) ;
+        }
+        # var_dump('BWA packageDownload 2', $_ENV, $temp_exe_file) ;
+        $loggingFactory = new \Model\Logging();
+        $logging = $loggingFactory->getModel($this->params);
+        $logging->log("Downloading From {$remote_source}", $this->getModuleName() ) ;
+
+        echo "Download Starting ...".PHP_EOL;
+        ob_start();
+        ob_flush();
+        flush();
+
+        $fp = fopen ($temp_exe_file, 'w') ;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $remote_source);
+        // curl_setopt($ch, CURLOPT_BUFFERSIZE,128);
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_PROGRESSFUNCTION, array($this, 'progress'));
+        curl_setopt($ch, CURLOPT_NOPROGRESS, false); // needed to make progress function work
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_exec($ch);
+        # $error = curl_error($ch) ;
+        # var_dump('downloaded', $downloaded, $error) ;
+        curl_close($ch);
+
+        ob_flush();
+        flush();
+
+        echo "Done".PHP_EOL ;
+        return $temp_exe_file ;
+    }
+
+    public function progress($resource, $download_size, $downloaded, $upload_size, $uploaded) {
+        $is_noprogress = (isset($this->params['noprogress']) ) ? true : false ;
+        if ($is_noprogress == false) {
+            if($download_size > 0) {
+                $dl = ($downloaded / $download_size)  * 100 ;
+                # var_dump('downloaded', $dl) ;
+                $perc = round($dl, 2) ;
+                # var_dump('perc', $perc) ;
+                echo "{$perc} % \r" ;
+            }
+        } else {
+            if($download_size > 0) {
+                $dl = ($downloaded / $download_size)  * 100 ;
+                # var_dump('downloaded', $dl) ;
+                $perc = round($dl) ;
+                # var_dump('perc', $perc) ;
+
+                if ($perc !== $this->cur_progress) {
+                    echo "{$perc} %  \r\n" ;
+                    $this->cur_progress = $perc ;
+                }
+
+//                $is_five_multiple = (is_int($perc / 5)) ? true : false ;
+////                $fm = fmod($perc, 1) ;
+////                $is_five_multiple = (is_int($fm)) ? true : false ;
+//                if ($is_five_multiple) {
+////                    echo "$fm\n" ;
+//                }
+            }
+        }
+        ob_flush();
+        flush();
+    }
+
 
     protected function attemptBoxRemove() {
         $loggingFactory = new \Model\Logging();
