37c1252c1900d6e036745db44d13c6d21ce32bdc
David Amanshia <phpengine@pharaohtools.com>
2019-05-01 18:25:54 +0100

    remove unused dumps

--- src/Modules/PTConfigureRequired/Model/BaseModelFactory.php
+++ src/Modules/PTConfigureRequired/Model/BaseModelFactory.php
@@ -7,6 +7,7 @@ class BaseModelFactory {
     public static function getModel($params, $modelGroup = "Default") {
         $thisModule = substr(get_called_class(), 6) ;
         $model = \Model\SystemDetectionFactory::getCompatibleModel($thisModule, $modelGroup, $params);
+//        var_dump('$model', $model, '$params', $params) ;
         return $model;
     }
 
